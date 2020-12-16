ec9d20c46f7bc7add6f5c16681d1e6c7129102a9
codingsb18 <monologi@naver.com>
2020-12-16 23:31:10 +0900

    삭제

--- Python_gui_basic/1_create_frame.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-root = Tk()
-root.title("Basic GUI") 
-root.geometry("640x480")    #창의 크기 설정 ( 가로 x 세로 + x좌표 + y좌표 )
-root.resizable(False, False)    #임의 크기조절 ( 너비, 높이 ) 기본값: True
-
-root.mainloop()
